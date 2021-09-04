719f3e716c086e5c61123fb7016305a2a0a6f734
Facundo Aguilar <facundoaguilar96@hotmail.com>
2021-09-04 00:02:23 -0300

    cuadrado mas loco

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function main() {
   const el = document.querySelector(".cuadrado");
+  const body = document.body;
 
   document.addEventListener("keydown", (event) => {
     const text = el.style.marginLeft.replace("px", "");
@@ -25,6 +26,21 @@ function main() {
     } else if (event.code == "ArrowDown") {
       newHeight--;
       newWidth--;
+    } else if (event.code == "KeyB") {
+      el.style.backgroundColor = "#0000ff";
+    } else if (event.code == "KeyR") {
+      el.style.backgroundColor = "#ff0000";
+    } else if (event.code == "KeyG") {
+      el.style.backgroundColor = "#00ff00";
+    } else if (event.code == "KeyY") {
+      el.style.backgroundColor = "#FFF700";
+    } else if (event.code == "KeyV") {
+      el.style.backgroundColor = "#E000FF";
+    } else if (event.code == "KeyO") {
+      el.style.backgroundColor = "#FF8F00";
+    } else if (event.code == "KeyW") {
+      el.style.backgroundColor = "#ffffff";
+      body.style.backgroundColor = "#000000";
     }
 
     el.style.width = newWidth + "px";
